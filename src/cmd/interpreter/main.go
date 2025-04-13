e38c25174c8c153f123390be86e9fd85266d363f
Mao De Matos <mao.dematos@gmail.com>
2025-04-14 00:22:26 +0200

    ✨ Add simple implementation

--- src/cmd/interpreter/main.go
+++ src/cmd/interpreter/main.go
@@ -1,5 +1,78 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+const FILE_PATH = "./examples/mandelbrot.bf"
+
+var data [30000]byte
+var curr int
+
+func main() {
+	fileContents, err := os.ReadFile(FILE_PATH)
+	check(err)
+
+	for i := 0; i < len(fileContents); i++ {
+		switch fileContents[i] {
+		case '<':
+			curr--
+		case '>':
+			curr++
+
+		case '+':
+			data[curr]++
+		case '-':
+			data[curr]--
+
+		case ',':
+			reader := bufio.NewReader(os.Stdin)
+			input, err := reader.ReadByte()
+			check(err)
+			data[curr] = input
+		case '.':
+			fmt.Printf("%c", data[curr])
+
+		case '[':
+			if data[curr] == 0 {
+				jump(Forward, &i, fileContents)
+			}
+		case ']':
+			if data[curr] != 0 {
+				jump(Backward, &i, fileContents)
+			}
+		}
+	}
+
+	return
+}
+
+type Direction = int
+
+const (
+	Forward  Direction = 1
+	Backward Direction = -1
+)
+
+func jump(direction Direction, i *int, fileContents []byte) {
+	var bracketCount int = 1
+
+	for bracketCount != 0 {
+		*i += direction
+
+		switch fileContents[*i] {
+		case '[':
+			bracketCount += direction
+		case ']':
+			bracketCount -= direction
+		}
+	}
 }
